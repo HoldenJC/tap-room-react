3355cdfc6953806a898bc2ebd673eb76887db503
HoldenJC <holdenj_c@yahoo.com>
2019-09-13 11:29:08 -0700

    update app.jsx file to prepare for state integration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,71 @@
 import React from 'react';
 import Header from './Header';
 import KegList from './KegList';
-import { Switch, Route } from 'react-router-dom';
-import NewKegForm from './NewKegForm';
+import NewKegControl from './NewKegControl';
 import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
+import Moment from 'moment';
+import Admin from './Admin';
+import { v4 } from 'uuid';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={KegList} />
-        <Route path='/newkeg' component={NewKegForm} />
-        <Route component={Error404} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterKegList: {},
+      selectedKeg: null
+    };
+    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
+    this.handleChangingSelectedKeg = this.handleChangingSelectedKeg.bind(this);
+  }
+
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateKegElapsedWaitTime(),
+      60000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.waitTimeUpdateTimer);
+  }
+
+  updateKegElapsedWaitTime() {
+    var newMasterKegList = Object.assign({}, this.state.masterKegList);
+    Object.keys(newMasterKegList).forEach(kegId => {
+      newMasterKegList[kegId].formattedWaitTime = (newMasterKegList[kegId].timeOpen).fromNow(true);
+    });
+    this.setState({ masterKegList: newMasterKegList });
+  }
+
+  handleAddingNewKegToList(newKeg) {
+    var newKegId = v4();
+    var newMasterKegList = Object.assign({}, this.state.masterKegList, {
+      [newKegId]: newKeg
+    });
+    newMasterKegList[newKegId].formattedWaitTime = newMasterKegList[newKegId].timeOpen.fromNow(true);
+    this.setState({ masterKegList: newMasterKegList });
+  }
+
+  handleChangingSelectedKeg(kegId) {
+    this.setState({ selectedKeg: kegId });
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' render={() => <KegList kegList={this.state.masterKegList} />} />
+          <Route path='/newkeg' render={() => <NewKegControl onNewKegCreation={this.handleAddingNewKegToList} />} />
+          <Route path='/admin' render={(props) => <Admin kegList={this.state.masterKegList} currentRouterPath={props.location.pathname} onKegSelection={this.handleChangingSelectedKeg} selectedKeg={this.state.selectedKeg} />} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
